35851a302f3a00b1f6241bbcc59488778b2fb01f
Ussama Azam <nuclearoreo@gmail.com>
2024-09-22 21:13:53 -0400

    Modifying codecrafter files

--- .codecrafters/run.sh
+++ .codecrafters/run.sh
@@ -6,4 +6,8 @@
 #
 # Learn more: https://codecrafters.io/program-interface
 
+# Set RUST_LOG to info to see logs
+export RUST_LOG=info
+
+# Run the program
 exec /tmp/codecrafters-build-dns-server-rust/release/codecrafters-dns-server "$@"
