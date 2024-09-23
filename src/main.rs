accd15dbf79447d4a2bc99ecbaa6671fe249a6e3
Ussama Azam <nuclearoreo@gmail.com>
2024-09-22 21:09:00 -0400

    Adding logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// External crates
+use env_logger;
+use log;
+
+// Internal modules
 mod dns;
 
 use dns::{Answer, DnsMessage, Header, Question};
@@ -7,8 +12,8 @@ const IP: &str = "127.0.0.1";
 const PORT: u16 = 2053;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    env_logger::init();
+    log::info!("Starting DNS server");
 
     let dns_message = DnsMessage {
         header: Header {
@@ -41,21 +46,22 @@ fn main() {
         }],
     };
 
-    // Uncomment this block to pass the first stage
     let udp_socket = UdpSocket::bind(format!("{IP}:{PORT}")).expect("Failed to bind to address");
+    log::info!("Listening on {IP}:{PORT}");
+
     let mut buf = [0; 512];
 
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
-                println!("Received {} bytes from {}", size, source);
+                log::info!("Received {} bytes from {}", size, source);
                 let response = dns_message.into_bytes();
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
             }
             Err(e) => {
-                eprintln!("Error receiving data: {}", e);
+                log::error!("Error receiving data: {}", e);
                 break;
             }
         }
